8ca14035322746250eb527344650cb65c6626ff3
kshravi86 <kshravi86@gmail.com>
2018-06-09 20:27:02 +0530

    skdf ks

--- js/admob.js
+++ js/admob.js
@@ -1,8 +1,8 @@
 var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
-        banner: 'ca-app-pub-5830283354936773/9003436870',
-       interstitial: 'ca-app-pub-5830283354936773/2601680056'
+        banner: 'ca-app-pub-5830283354936773/2492236554',
+       interstitial: 'ca-app-pub-5830283354936773/5394689929'
 	   
 	   
     };
